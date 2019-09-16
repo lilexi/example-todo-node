e72bc125b6c7ee27773078a525aeb3cb67209bf3
Sp <spherastree@gmail.com>
2019-09-16 20:40:07 +0300

    init > mongodb connect

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://lilexi:testM0ng0@cluster0-iwb70.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(
+  uri,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+);
+client.connect(err => {
+  if (err) {
+    console.log(err);
+  }
+  const collection = client.db("example-todo").collection("users");
+  console.log("[ ✅  ] Mongodb: connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+module.exports = router;
